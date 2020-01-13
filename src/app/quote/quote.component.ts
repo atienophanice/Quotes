3cdde750f221d445e81df36183692e48899f970c
Phanice <maureezgaga@gmail.com>
2020-01-13 12:29:25 +0300

    added component qoute

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { Quote} from './quote';
+import { Quote} from '../quote'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.scss']
 })
-
-export class AppComponent{
+export class QuoteComponent implements OnInit {
   quotes:Quote[]= [
     {id:1, name:'Act as if what you do makes a difference .It does'},
     {id:2,name:'Limit your ALWAYS and your NEVERS'},
     {id:3,name:'Never bend your head.Always hold it high.Look the world straight in the eye'},
   ];
-  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
