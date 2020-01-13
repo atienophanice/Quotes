d22e412292087ba1ba0d46e5699a877d5d9f577f
Phanice <maureezgaga@gmail.com>
2020-01-13 14:43:21 +0300

    imported qoute app

--- src/app/qoute-details/qoute-details.component.ts
+++ src/app/qoute-details/qoute-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Quote} from '../quote';
 @Component({
   selector: 'app-qoute-details',
   templateUrl: './qoute-details.component.html',
